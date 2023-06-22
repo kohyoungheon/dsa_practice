8d74cd32c1eff9f398229daed16aeecc510c4668
Young Koh <youngheon6@gmail.com>
2023-06-22 16:21:35 -0600

    feat: has_path

--- 4. graphs.rb
+++ 4. graphs.rb
@@ -1,3 +1,4 @@
+#n = number of nodes, O(n) time, O(n) space
 def depth_first_print(graph, start)
   stack = [ start ]
   while stack.length > 0
@@ -10,6 +11,7 @@ def depth_first_print(graph, start)
   end
 end
 
+#n = number of nodes, O(n) time, O(n) space
 def depth_first_print_rec(graph, current)
   puts current
   graph[current].each do |neighbor|
@@ -17,6 +19,49 @@ def depth_first_print_rec(graph, current)
   end
 end
 
-def breadth_first_print
-  
+#n = number of nodes, O(n) time, O(n) space
+def breadth_first_print(graph, start)
+  queue = [ start ]
+  while queue.length > 0
+    current = queue[0]
+    puts current
+    queue.shift
+    graph[current].each do |neighbor|
+      queue.push(neighbor)
+    end
+  end
+end
+#_______________________________________________________________________________
+#Depth first recursive
+#n = number of nodes, e = number edges, Time: O(e), Space: O(n)
+def has_path(graph, src, dst)
+  return true if src == dst
+  graph[src].each do |neighbor|
+    if has_path(graph, neighbor, dst) == true
+      return true
+    end
+  end
+
+  return false
+end
+
+#Breadth first
+#n = number of nodes, e = number edges, Time: O(e), Space: O(n)
+def has_path_breadth(graph, src, dst)
+  queue = [ src ]
+
+  while queue.length > 0
+    current = queue.shift
+
+    return true if current == dst
+
+    graph[current].each do |neighbor|
+      queue.push(neighbor)
+    end
+  end
+  return false
+end
+#_______________________________________________________________________________
+def undirected_path(edges, node_A, node_B)
+
 end
--- spec/4. graphs_spec.rb
+++ spec/4. graphs_spec.rb
@@ -2,6 +2,7 @@ require './4. graphs.rb'
 require 'rspec'
 
 describe "graphs" do
+
   describe "depth first print" do
     it "test case 1" do
       graph = {
@@ -16,6 +17,7 @@ describe "graphs" do
       expect { depth_first_print(graph, "a") }.to output("a\nc\ne\nb\nd\nf\n").to_stdout
     end
   end
+
   describe "depth first print recursive" do
     it "test case 1" do
       graph = {
@@ -33,7 +35,102 @@ describe "graphs" do
 
   describe "breadth first print" do
     it "test case 1" do
+      graph = {
+        "a" => ["b", "c"],
+        "b" => ["d"],
+        "c" => ["e"],
+        "d" => ["f"],
+        "e" => [],
+        "f" => []
+      }
 
+      expect { breadth_first_print(graph, "a") }.to output("a\nb\nc\nd\ne\nf\n").to_stdout
+    end
+  end
+
+  describe "has_path" do
+    it "test case 1" do
+      graph = {
+        'f'=> ['g', 'i'],
+        'g'=> ['h'],
+        'h'=> [],
+        'i'=> ['g', 'k'],
+        'j'=> ['i'],
+        'k'=> []
+      }
+      
+      # has_path(graph, 'f', 'k') # True
+      expect(has_path(graph, 'f', 'k')).to eq(true)
+    end
+
+    it "test case 2" do
+      graph = {
+        'f'=> ['g', 'i'],
+        'g'=> ['h'],
+        'h'=> [],
+        'i'=> ['g', 'k'],
+        'j'=> ['i'],
+        'k'=> []
+      }
+      
+      # has_path(graph, 'f', 'j') # False
+      expect(has_path(graph, 'f', 'j')).to eq(false)
+    end
+
+    it "test case 3" do
+      graph = {
+        'v'=> ['x', 'w'],
+        'w'=> [],
+        'x'=> [],
+        'y'=> ['z'],
+        'z'=> [],  
+      }
+      
+      # has_path(graph, 'v', 'w') # True
+      expect(has_path_breadth(graph, 'v', 'w')).to eq(true)
+    end
+  end
+
+  describe "undirected_path" do
+    it "test case 1" do
+      edges = [
+        ('i', 'j'),
+        ('k', 'i'),
+        ('m', 'k'),
+        ('k', 'l'),
+        ('o', 'n')
+      ]
+      
+      # undirected_path(edges, 'j', 'm') # -> True
+      expect(undirected_path(edges, 'j', 'm')).to eq(true)
+    end
+    it "test case 2" do
+      edges = [
+        ('i', 'j'),
+        ('k', 'i'),
+        ('m', 'k'),
+        ('k', 'l'),
+        ('o', 'n')
+      ]
+      
+      # undirected_path(edges, 'k', 'o') # -> False
+      expect(undirected_path(edges, 'k', 'o')).to eq(false)
+    end
+
+    it "test case 3" do
+      edges = [
+        ('b', 'a'),
+        ('c', 'a'),
+        ('b', 'c'),
+        ('q', 'r'),
+        ('q', 's'),
+        ('q', 'u'),
+        ('q', 't'),
+      ]
+      
+      
+      # undirected_path(edges, 'a', 'b') # -> True
+      expect(undirected_path(edges, 'a', 'b')).to eq(true)
     end
   end
 end
