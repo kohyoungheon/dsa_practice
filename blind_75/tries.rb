14c5f39ef92424366d233b992772a07301d613a6
Young Koh <youngheon6@gmail.com>
2023-08-18 13:55:08 -0600

    tries are confusing

--- blind_75/tries.rb
+++ blind_75/tries.rb
@@ -45,40 +45,41 @@ end
 
 class WordDictionary
   def initialize
-    @root = Trie.new
+    @root = TrieNode.new
   end
 
   def add_word(word)
     current = @root
     word.each_char do |char|
-      if !current[children][char]
-        current.children[char] = TrieNode.new
-      end
+      current.children[char] = TrieNode.new unless current.children[char]
       current = current.children[char]
     end
     current.is_end = true
   end
 
   def search(word)
-    def dfs(j, root)
-      current = root
-      word.length.times do |i|
-        char = word[i]
-        if char = '.'
-          current.children.values.each do |child|
-            if dfs(i + 1, child)
-              return true
-            end
-            return false
-          end
-        else
-          return false unless current.children[char]
-          current = current.children[char]
-        end
-      end
-    end
+    dfs(0, word, @root)
   end
 
+  private
+
+  def dfs(j, word, current)
+    return true if j == word.length && current.is_end
+
+    return false if j >= word.length
+
+    char = word[j]
+    if char == '.'
+      current.children.values.each do |child|
+        return true if dfs(j + 1, word, child)
+      end
+    else
+      return false unless current.children[char]
+      return dfs(j + 1, word, current.children[char])
+    end
+
+    false
+  end
 
 end
 
