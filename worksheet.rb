8fc473e2c7ccdca02f7559931ed3388901d2d4e9
Young Koh <youngheon6@gmail.com>
2023-09-16 15:36:17 -0600

    2 pointers

--- worksheet.rb
+++
@@ -1,9 +0,0 @@
-def replace_elements(arr)
-  right_max = -1
-  (arr.length - 1).downto(0) do |i|
-    new_max = [right_max, arr[i]].max
-    arr[i] = right_max
-    right_max = new_max
-  end
-  arr
-end
